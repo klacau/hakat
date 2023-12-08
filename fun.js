3706b623a08b0ba864617d8839082c51b5f13270
Mi <you@example.com>
2023-12-08 15:28:13 +0300

    обновил

--- fun.js
+++ fun.js
@@ -2,7 +2,6 @@ import SQLite from 'react-native-sqlite-storage';
 
 const db = SQLite.openDatabase({ name: 'myDatabase.db', location: 'default' });
 
-<<<<<<< HEAD
 // Использование хука useFocusEffect для автоматической загрузки данных при фокусировке на экране
 useFocusEffect(
   React.useCallback(() => {
@@ -34,24 +33,3 @@ useFocusEffect(
     fetchOrders();
   }, [])
 );
-=======
-const fetchOrders = async () => {
-  try {
-    db.transaction((tx) => {
-      tx.executeSql(
-        'SELECT * FROM orders',
-        [],
-        (_, { rows }) => {
-          const ordersData = rows.raw();
-          setOrders(ordersData);
-        },
-        (tx, error) => {
-          console.error('Error fetching orders', error);
-        }
-      );
-    });
-  } catch (error) {
-    console.error('Error fetching orders', error);
-  }
-};
->>>>>>> aca16120a1df644ae189a8492f5405f811b6c89a
